f65330739415ff26096948fb3b55ee00f84a033c
Martinez Garrido Angel Ivan <mgangelivan@gmail.com>
2022-01-04 18:56:23 -0600

    se incorporo la logica a la pagina de inicio

--- src/components/inicio/inicio.js
+++ src/components/inicio/inicio.js
@@ -1,7 +1,9 @@
 import './inicio.scss';
 import INICIO from './inicio.html';
+import { changeColor } from '../../utils/changeColor';
 
 export const loadInicio = () => {
   const header = document.querySelector('.header');
   header.insertAdjacentHTML('afterend', INICIO);
+  changeColor();
 };
